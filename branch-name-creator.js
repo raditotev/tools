3158db99cacf558ddecfeeeecfcae1afe15f162e
Radi Totev <raditotev@gmail.com>
2023-04-02 15:25:54 +0300

    Update branch-name-creator

--- branch-name-creator.js
+++ branch-name-creator.js
@@ -1,3 +1,11 @@
-console.log(
- process.argv[2].toLowerCase().split(' ').join('-')
-);
+#!/usr/bin/env node
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+readline.question(`Enter branch name: `, (name) => {
+  console.log('=> ', name.toLowerCase().split(' ').join('-'))
+  readline.close()
+})
